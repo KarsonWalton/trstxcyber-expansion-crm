16fdad959d502b415a37f545b5be74c15960ccae
KarsonWestall <westall.karson@gmail.com>
2026-07-07 11:58:32 -0700

    ok

--- api/config.js
+++ api/config.js
@@ -1,11 +1,9 @@
 module.exports = function handler(req, res) {
-  // Enable CORS headers so your frontend can fetch it
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET');
 
-  // Send the back-end environment variables securely
   res.status(200).json({
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY
+    supabaseUrl: process.env.SUPABASE_URL,
+    supabaseAnonKey: process.env.SUPABASE_ANON_KEY
   });
 };
